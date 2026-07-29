08f1a99f846c3dacca435c63d3f4a3e748ef225a
tracer-ics2025 <tracer@njuics.org>
2026-07-29 17:33:48 +0800

    >  compile NEMU 240210210 xuan Linux xuan-Standard-PC-Q35-ICH9-2009 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:33:47 up 1 day,  2:08,  1 user,  load average: 0.89, 0.55, 0.48

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,6 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include "utils.h"
 
 static int is_batch_mode = false;
 
@@ -50,6 +51,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
