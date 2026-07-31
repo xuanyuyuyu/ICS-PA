d0a57e1a013a89d4c5585abdcdcd500cddbfd175
tracer-ics2025 <tracer@njuics.org>
2026-07-31 13:06:17 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:06:16 up 1 day, 15:41,  1 user,  load average: 0.49, 0.41, 0.57

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -27,56 +27,64 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-// int main(int argc, char *argv[]) {
-//   /* Initialize the monitor. */
-// #ifdef CONFIG_TARGET_AM
-//   am_init_monitor();
-// #else
-//   init_monitor(argc, argv);
-// #endif
+int main(int argc, char *argv[]) {
+  /* Initialize the monitor. */
+#ifdef CONFIG_TARGET_AM
+  am_init_monitor();
+#else
+  init_monitor(argc, argv);
+#endif
 
-//   /* Start engine. */
-//   engine_start();
+  /* Start engine. */
+  engine_start();
 
 
-//   return is_exit_status_bad();
-// }
-
-int main(int argc, char **argv) {
-  FILE *fp = fopen("input", "r");
-  if(fp == NULL) {
-    perror("input error");
-    return 1;
-  }
-
-  init_regex();
-
-  unsigned expected;
-  char expression[65536];
-
-  while(fscanf(fp, "%u %[^\n]", &expected, expression) == 2) {
-    bool success = false;
-
-    printf("============================即将开始计算\n");
-    printf("expr    = %s\ns", expression);
-
-    word_t actual = expr(expression, &success);
-
-    if(!success || actual != expected) {
-      printf("=================================\n");
-      printf("Mismatch!\n");
-      printf("expected = %u\n", expected);
-      printf("actual   = %u\n", actual);
-      printf("expr     = %s\n", expression);
-      printf("=================================\n");
-      break;
-    }
-
-    printf("PASS: %u %s\n", actual, expression);
-
-  }
-
-  fclose(fp);
-
-  return 0;
+  return is_exit_status_bad();
 }
+
+
+
+
+
+
+
+
+
+// int main(int argc, char **argv) {
+//   FILE *fp = fopen("input", "r");
+//   if(fp == NULL) {
+//     perror("input error");
+//     return 1;
+//   }
+
+//   init_regex();
+
+//   unsigned expected;
+//   char expression[65536];
+
+//   while(fscanf(fp, "%u %[^\n]", &expected, expression) == 2) {
+//     bool success = false;
+
+//     printf("============================即将开始计算\n");
+//     printf("expr    = %s\ns", expression);
+
+//     word_t actual = expr(expression, &success);
+
+//     if(!success || actual != expected) {
+//       printf("=================================\n");
+//       printf("Mismatch!\n");
+//       printf("expected = %u\n", expected);
+//       printf("actual   = %u\n", actual);
+//       printf("expr     = %s\n", expression);
+//       printf("=================================\n");
+//       break;
+//     }
+
+//     printf("PASS: %u %s\n", actual, expression);
+
+//   }
+
+//   fclose(fp);
+
+//   return 0;
+// }
