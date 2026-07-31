0e35429c4c59c844190e7161eded7a4d07e46b94
tracer-ics2025 <tracer@njuics.org>
2026-07-31 12:51:16 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:51:16 up 1 day, 15:26,  1 user,  load average: 1.44, 0.98, 0.68

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -13,6 +13,7 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +24,7 @@
 
 // this should be enough
 static char buf[65536] = {};
+static char cbuf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -37,9 +39,13 @@ static int choose(int n) {
 }
 
 static void gen(char c) {
-  int len = strlen(buf);
+  size_t len = strlen(buf);
   buf[len] = c;
   buf[len + 1] = '\0';
+
+  len = strlen(cbuf);
+  cbuf[len] = c;
+  cbuf[len + 1] = '\0';
 }
 
 
@@ -48,10 +54,13 @@ static bool valid;
 static unsigned gen_rand_expr() {
   switch(choose(3)) {
     case 0: {
-      unsigned value = rand() % 100;
-      char num[20];
+      unsigned value = (unsigned)rand() % 100;
+      char num[32];
       sprintf(num, "%u", value);
       strcat(buf, num);
+
+      sprintf(num, "%uu", value);
+      strcat(cbuf, num);
       return value;
     }
     case 1: {
@@ -75,8 +84,8 @@ static unsigned gen_rand_expr() {
       switch (op) {
         case '+': return op1 + op2;
         case '-': return op1 - op2;
-        case '*': return op1 * op2;
         case '/': return op1 / op2;
+        case '*': return op1 * op2;
       }
 
     }
@@ -94,16 +103,17 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    int result;
+    unsigned result;
     do{
       buf[0] = '\0';
+      cbuf[0] = '\0';
       valid = true;
-      result = gen_rand_expr();
+      result = (unsigned)gen_rand_expr();
     } while(!valid);
       
 
     //把 buf（表达式字符串，如 1 + 2 * 3）嵌入到 code_format 这个 C 程序模板里，结果写入 code_buf
-    sprintf(code_buf, code_format, buf);
+    sprintf(code_buf, code_format, cbuf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
