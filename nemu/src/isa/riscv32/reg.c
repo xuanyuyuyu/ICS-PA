bf031868431c8df7a06920dac194989b78ff4a8f
tracer-ics2025 <tracer@njuics.org>
2026-07-31 14:31:46 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:31:45 up 1 day, 17:06,  1 user,  load average: 0.57, 0.55, 0.45

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -42,7 +42,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
       return cpu.gpr[i];
     }
   }
-  if(strcmp(s, "pc") == 0) {
+  if(strcmp(s, "$pc") == 0) {
       *success = true;
       return cpu.pc;
   }
