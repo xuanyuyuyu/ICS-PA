98770b3a1e1f4bed49b80890d955599c53fcf85f
tracer-ics2025 <tracer@njuics.org>
2026-08-07 13:38:49 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:38:47 up 5 days, 21:16,  1 user,  load average: 0.48, 0.43, 0.31

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -13,17 +13,25 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "common.h"
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if(direction == DIFFTEST_TO_DUT) {
+    memcpy(buf, guest_to_host(addr), n);
+  } else if(direction == DIFFTEST_TO_REF) {
+    memcpy(guest_to_host(addr), buf, n);
+  }
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  
+
+
+
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
