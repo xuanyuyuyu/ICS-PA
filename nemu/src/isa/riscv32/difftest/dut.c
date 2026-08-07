5000d9ad650b4813205449cd22f6b0471b68b0fa
tracer-ics2025 <tracer@njuics.org>
2026-08-07 13:42:36 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:42:34 up 5 days, 21:20,  1 user,  load average: 1.13, 0.77, 0.48

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if(cpu.pc != ref_r->pc) {
+  if(pc != ref_r->pc) {
     return false;
   }
   for(int i = 0; i < 32; i ++) {
