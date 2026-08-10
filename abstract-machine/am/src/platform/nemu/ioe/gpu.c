45a9ee3b0e05796ef3d878f218d6342d021b368f
tracer-ics2025 <tracer@njuics.org>
2026-08-10 14:58:49 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:58:48 up 8 days, 10:04,  1 user,  load average: 0.75, 0.37, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,17 +1,11 @@
 #include <am.h>
 #include <nemu.h>
 #include <stdint.h>
-
+#include <klib.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = 10;
-  int h = 150;
-
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for( i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+ 
 
 }
 
@@ -28,9 +22,31 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t config = inl(VGACTL_ADDR);
+  int screen_w = config >> 16;
+  int screen_h = config & 0xffff;
+
+  assert(ctl->x >= 0 && ctl->y >= 0);
+  assert(ctl->w >= 0 && ctl->h >= 0);
+  assert(ctl->x + ctl->w <= screen_w);
+  assert(ctl->y + ctl->h <= screen_h);
+
+  volatile uint32_t *fb = (volatile uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+
+
+  for (int y = 0; y < ctl->h; y++) {
+    for (int x = 0; x < ctl->w; x++) {
+      int dst = (ctl->y + y) * screen_w + (ctl->x + x);
+      int src = y * ctl->w + x;
+
+      fb[dst] = pixels[src];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
