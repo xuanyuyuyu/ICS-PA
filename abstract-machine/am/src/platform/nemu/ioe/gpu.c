8394688131f044c141609ba18369ba53d49cbfc5
tracer-ics2025 <tracer@njuics.org>
2026-08-10 13:46:28 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:46:26 up 8 days,  8:52,  1 user,  load average: 0.90, 0.55, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,8 +6,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = 1000;
-  int h = 1500;
+  int w = 10;
+  int h = 150;
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for( i = 0; i < w * h; i ++) fb[i] = i;
