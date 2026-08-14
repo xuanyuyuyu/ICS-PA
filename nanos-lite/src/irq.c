1a8079c28509a2e8cb0ce6f009f4156ff8f73df5
tracer-ics2025 <tracer@njuics.org>
2026-08-14 15:45:49 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-136-generic #136~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  3 16:29:11 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:45:48 up 2 days,  2:19,  1 user,  load average: 1.24, 0.86, 0.59

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,9 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD :
+      printf("EVENT_YIELD is trapped");
+
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,11 +6,6 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case SYS_yield:
-      yield();
-      c->GPR1 = 0;
-      break;
-      
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
