64ca853306a49010045652546be37ba71aaf4852
tracer-ics2025 <tracer@njuics.org>
2026-08-30 15:07:28 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-138-generic #138~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:43:15 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:07:24 up 21 min,  1 user,  load average: 0.50, 0.79, 0.83

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -14,7 +14,7 @@ typedef struct {
   size_t open_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENTS};
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
