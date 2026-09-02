ee00668fc5bacea5f7b6b80cc006efe75104d408
tracer-ics2025 <tracer@njuics.org>
2026-09-02 22:32:51 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-138-generic #138~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:43:15 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:32:49 up 1 day, 11:26,  3 users,  load average: 0.91, 1.00, 1.17

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,7 +18,38 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+  if(event == NULL) {
+    return 0;
+  }
+
+  char buf[64];
+  while(1) {
+    if(NDL_PollEvent(buf, sizeof(buf)) == 0) {
+      continue;
+    }
+
+    // 返回的应该为"kd J\n"或"ku DOWN\n"
+    if(buf[0] != 'k' || (buf[1] != 'd' || buf[1] != 'u') || buf[2] != ' ') continue;
+  
+    char *name = buf + 3; //按键名
+
+    //去掉结尾换行符
+    char *newline = strchr(name, '\n');
+    if(newline != NULL) {
+      *newline = '\0';
+    }
+    for(int i = 0; i < (int)sizeof(keyname) / sizeof(keyname[0]); i ++) {
+      if(strcmp(name, keyname[i]) == 0) {
+        event->type = (buf[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+        event->key.keysym.sym = i;
+
+        return 1;
+      }
+    }
+
+
+  }
+  
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
