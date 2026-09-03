358b6c598c6f3a1cc340a9caa4456931f9078f16
tracer-ics2025 <tracer@njuics.org>
2026-09-03 13:29:23 +0800

    >  compile NEMU 240210210 xuan Linux xuandeVM 6.8.0-138-generic #138~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:43:15 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:29:20 up  3:25,  1 user,  load average: 0.80, 0.74, 0.71

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,10 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -12,7 +16,47 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename, "rb");
+  if(fp == NULL) {
+    return NULL;
+  }
+  //移动到文件末尾，用ftell获得文件大小
+  if(fseek(fp, 0, SEEK_END) != 0) {
+    fclose(fp);
+    return NULL;
+  }
+
+  long file_size = ftell(fp);  //ftell返回当前位置相对于文件开头的偏移量
+  if (file_size <= 0 || file_size > INT_MAX) {
+    return NULL;
+  }
+  //回到文件开头，准备读取文件
+  if(fseek(fp, 0, SEEK_SET) != 0) {
+    fclose(fp);
+    return NULL;
+  }
+
+  unsigned char *buf = (unsigned char *)malloc(file_size);
+  if(buf == NULL) {
+    fclose(fp);
+    return NULL;
+  }
+
+  //将整个文件读入buf
+  size_t nread = fread(buf, 1, file_size, fp);
+  fclose(fp);
+
+  //文件未能完整读出
+  if(nread != (size_t)file_size) {
+    free(buf);
+    return NULL;
+  }
+
+  SDL_Surface *surface = STBIMG_LoadFromMemory(buf, (int)file_size);
+
+  free(buf);
+
+  return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
